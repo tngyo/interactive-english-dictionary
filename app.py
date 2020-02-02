e5c829f786e5b4a293865d792eec1b2a3237778f
S.L. Ting <drikting@gmail.com>
2020-02-03 06:29:24 +0800

    Version 0.2

--- app.py
+++ app.py
@@ -7,18 +7,14 @@ data = json.load(open("data.json"))
 def define(word):
     closest_match = difflib.get_close_matches(word, data.keys(), 1)
     if closest_match:
-        print(f"\n{closest_match[0]}")
+        match_ratio = difflib.SequenceMatcher(None, word, closest_match[0]).ratio()
+        match_ratio = round(match_ratio, 2) * 100
+        if match_ratio < 100:
+            print(f'\nMatch ratio for {word} and {closest_match[0]} is: {match_ratio} percent')
+            print(f"\n{closest_match[0]}")
         prettify(data[closest_match[0]])
     else:
         print(f"\nSorry, {word} is not found in this dictionary.\n")
-        
-
-"""
-    try:
-        prettify(data[word])
-    except KeyError:
-        print("\nThis word is not defined in this local dictionary.\n")
-"""
 
 
 def prettify(list):
