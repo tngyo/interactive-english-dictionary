75ee154435a112999f6ab55972899c1862849eff
S.L. Ting <drikting@gmail.com>
2020-02-03 08:50:57 +0800

    version 0.3; Some coding touch up

--- app.py
+++ app.py
@@ -7,28 +7,27 @@ data = json.load(open("data.json"))
 def define(word):
     closest_match = difflib.get_close_matches(word, data.keys(), 1)
     if closest_match:
-        match_ratio = difflib.SequenceMatcher(None, word, closest_match[0]).ratio()
+        closest = closest_match[0]
+        match_ratio = difflib.SequenceMatcher(None, word, closest).ratio()
         match_ratio = round(match_ratio, 2) * 100
         if match_ratio < 100:
-            user_input = input("\nDo you mean %s instead? Press Y or N: " % closest_match[0].upper())
+            user_input = input("\nDo you mean %s instead? Press Y or N: " % closest.upper())
             if user_input == 'Y':
-                print(f'\nMatch ratio for {word} and {closest_match[0]} is: {match_ratio} percent')
-                print(f"\n{closest_match[0]}\n")
-                return prettify(data[closest_match[0]])
+                print(f'\n**The match ratio for {word} and {closest} is: {match_ratio} percent**')
+                print(f"\n{closest}\n")
+                return prettify(data[closest])
             elif user_input == 'N':
-                return "\nSorry word was not found in this dictionary.\n"
+                return "\nSorry, %s was not found in this dictionary.\n" % word
             else:
                 return "\nSorry this input is not understood.\n"
-        return prettify(data[closest_match[0]])
+        return prettify(data[closest])
     else:
-        return "\nSorry, {word} is not found in this dictionary.\n"
+        return "\nSorry, %s is not found in this dictionary.\n" % word
 
 
 def prettify(list):
-    numbering = 1
-    for line in list:
-        print(f'\n{numbering}. {line}')
-        numbering += 1
+    for numbering, line in enumerate(list):
+        print(f'\n{numbering+1}. {line}')
     return "\nDone! Thank you for learning English :-)\n"
 
 
