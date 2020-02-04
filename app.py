184cafbb828195173801332edec5316f2b15fbe5
S.L. Ting <drikting@gmail.com>
2020-02-04 10:07:59 +0800

    version 0.5. Added support for uppercased words such as USA and NATO

--- app.py
+++ app.py
@@ -5,20 +5,22 @@ data = json.load(open("data.json"))
 
 
 def define(word):
+    if word.capitalize() in data:
+       return prettify(data[word.capitalize()], word.capitalize())
+    if word.upper() in data:
+       return prettify(data[word.upper()], word.upper())
     closest_match = difflib.get_close_matches(word, data.keys(), 1)
     if closest_match:
         closest = closest_match[0]
         match_ratio = ratio(word, closest)
         if match_ratio < 100:
-            if ratio(word.capitalize(), closest) == 100:
-                return prettify(data[closest], closest)
-            user_input = input("\nDo you mean %s instead? Press Y or N: " % closest)
-            if user_input == 'Y':
+           user_input = input("\nDo you mean %s instead? Press Y or N: " % closest)
+           if user_input == 'Y':
                 print(f'\n**The match ratio for {word} and {closest} is: {match_ratio} percent**')
                 return prettify(data[closest], closest)
-            elif user_input == 'N':
+           elif user_input == 'N':
                 return "\nSorry, %s was not found in this dictionary.\n" % word
-            else:
+           else:
                 return "\nSorry this input is not understood.\n"
         return prettify(data[closest], closest)
     else:
