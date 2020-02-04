fa2e3334e4536aa119aa07b414d31d7a0e07aec7
S.L. Ting <drikting@gmail.com>
2020-02-04 09:30:55 +0800

    version 0.4; added recognition for special words with capitalization such as contries or cities names

--- app.py
+++ app.py
@@ -8,24 +8,30 @@ def define(word):
     closest_match = difflib.get_close_matches(word, data.keys(), 1)
     if closest_match:
         closest = closest_match[0]
-        match_ratio = difflib.SequenceMatcher(None, word, closest).ratio()
-        match_ratio = round(match_ratio, 2) * 100
+        match_ratio = ratio(word, closest)
         if match_ratio < 100:
-            user_input = input("\nDo you mean %s instead? Press Y or N: " % closest.upper())
+            if ratio(word.capitalize(), closest) == 100:
+                return prettify(data[closest], closest)
+            user_input = input("\nDo you mean %s instead? Press Y or N: " % closest)
             if user_input == 'Y':
                 print(f'\n**The match ratio for {word} and {closest} is: {match_ratio} percent**')
-                print(f"\n{closest}\n")
-                return prettify(data[closest])
+                return prettify(data[closest], closest)
             elif user_input == 'N':
                 return "\nSorry, %s was not found in this dictionary.\n" % word
             else:
                 return "\nSorry this input is not understood.\n"
-        return prettify(data[closest])
+        return prettify(data[closest], closest)
     else:
         return "\nSorry, %s is not found in this dictionary.\n" % word
 
 
-def prettify(list):
+def ratio(word, closest):
+    match_ratio = difflib.SequenceMatcher(None, word, closest).ratio()
+    return round(match_ratio, 2) * 100
+
+
+def prettify(list, closest):
+    print(f"\n{closest}\n")
     for numbering, line in enumerate(list):
         print(f'\n{numbering+1}. {line}')
     return "\nDone! Thank you for learning English :-)\n"
